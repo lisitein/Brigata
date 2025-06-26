c37cd09802de00ceaba31d1a11641e56c946a3df
lisitein <167685239+lisitein@users.noreply.github.com>
2025-06-26 10:54:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ relational_db = "journals.db"
 graph_db = "http://127.0.0.1:9999/blazegraph/sparql"
 
 # Upload data (do this once)
-journal_uploader = JournalUploadHandler()
+journal_uploader = JournalUploadHandler()  
 journal_uploader.setDbPathOrUrl(graph_db)
-journal_uploader.pushDataToDb("data/doaj.csv")
+journal_uploader.pushDataToDb("doaj.csv")  
 
 category_uploader = CategoryUploadHandler()
-category_uploader.setDbPathOrUrl(relational_db)
-category_uploader.pushDataToDb("data/scimago.json")
+category_uploader.setDbPathOrUrl(relational_db)  
+category_uploader.pushDataToDb("scimago.json")  
 
 # Create query engine
 journal_query = JournalQueryHandler()
@@ -29,5 +29,5 @@ all_journals = engine.getAllJournals()
 open_access = engine.getJournalsWithDOAJSeal()
 
 # Get results
-for journal in science_journals[:5]:
+for journal in science_journals[:5]:  
     print(journal.getTitle())
