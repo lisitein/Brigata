a9d512ee48e9cfd974b5ffb25e5e7356bb898019
Yang Tianchi <yangtianchi2017@gmail.com>
2025-06-26 10:39:25 +0200

    Update impl.py

--- impl.py
+++ impl.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from laura import Journal, Category, Area, BasicQueryEngine, FullQueryEngine
-from daniele import CategoryUploadHandler, CategoryQueryHandler
+from daniele import CategoryUploadHandler
 from li import JournalUploadHandler
-from Yang import JournalQueryHandler
+from Yang import JournalQueryHandler, CategoryQueryHandler
 
 __all__ = [
     "Handler", "UploadHandler", "JournalUploadHandler", "CategoryUploadHandler",
