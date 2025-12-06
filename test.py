66bf76f88d31bda28d9fb610b603413b952c22be
lisitein <167685239+lisitein@users.noreply.github.com>
2025-12-06 14:16:40 +0100

    Refactor tests for entity and journal retrieval
    Updated test cases for getEntityById and getAllJournals functions with correct inputs and outputs.

--- test.py
+++ test.py
@@ -20,24 +20,28 @@ correct_inputs_and_outputs=[
 def test_getEntityById(correct_inputs_and_outputs):
     for pair in correct_inputs_and_outputs:
         if getEntityById(pair["input"])==pair["output"]:
-            print(True)
-    
-
+            print("Yoho^^ getEntityById is True")
+        else:
+            print("Eha... The result of test_getEntityById (TEST1) is uncorrect.")
 
 
 
 #TEST 2
 
 correct_inputs_and_outputs=[
-    {"input":     ,
-     "output":       }
-
+    Journal(title = "Prolíngua", id = "1983-9979", language = "Portuguese", publisher = "Universidade Federal da Paraíba", seal=False, license="CC BY", apc=True),
+    Journal(title = "Проблеми Законності", id = ["2224-9281","2414-990X"] language = ["Ukrainian", "Russian", "English"], publisher = "Yaroslav Mudryi National Law University", seal=False, license="CC BY", apc=True),
+    Journal(title = "Enlightening Tourism", id = "2174-548X", language = "English", publisher = "University of Huelva", seal=False, license="CC BY", apc=True),
+    Journal(title = "Scientific Journals of the Maritime University of Szczecin", id = ["1733-8670","2392-0378"], language = "English", publisher = "MUS", seal=False, license="CC BY", apc=True),
+    Journal(title = "Fronteiras: Journal of Social, Technological and Environmental Science", id = "2238-8869", language = "Portuguese", publisher = "Centro Universitário de Anápolis", seal=False, license="CC BY", apc=True),
+    Journal(title = "Semina: Ciências Agrárias", id = ["1676-546X","1679-0359"], language = ["Portuguese", "English"], publisher = "Universidade Estadual de Londrina", seal=False, license="CC BY", apc=True),
     ]
 
 def test_getAllJournals():
-    for pair in correct_inputs_and_outputs:
-        if getAllJournals(pair["input"])==pair["output"]:
-            print(True)
+    if getAllJournals()== correct_inputs_and_outputs:
+        print("Yoho!^^ getAllJournals is True")
+    else:
+        print("Eha... The result of getAllJournals (TEST2) is uncorrect.")
 
 
 
@@ -93,4 +97,5 @@ def test_getJournalsInAreasWithLicense():
     pass
 
 def test_getDiamondJournalsInAreasAndCategoriesWithQuartile():
+
     pass
