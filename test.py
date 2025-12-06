0adee93bf6c4017657a3b99b6284a0e33f3c1966
danielecamagnadhdk <daniele.camagna@studio.unibo.it>
2025-12-06 13:11:09 +0100

    v

--- test.py
+++ test.py
@@ -4,9 +4,44 @@ from li import *
 from Yang import *
 from baseHandler import *
 
-def test_getEntityById(id, expected):
-    result=getEntityById()
-    pass
+
+
+#TEST 1
+
+correct_inputs_and_outputs=[
+    ("1474-1784", Journal(id=["1474-1784","1474-1776"]))
+    ("santa-claus", None)
+    ("2224-9281", Journal(id=["2224-9281", "2414-990X"], title="Проблеми Законності", languages=["Ukrainian", "Russian", "English"], publisher="Yaroslav Mudryi National Law University", seal=False, license="CC BY", apc=True))
+    ("happy-yang", None)
+    ("2224-9281", Journal(id=["2224-9281", "2414-990X"], title="Проблеми Законності", languages=["Ukrainian", "Russian", "English"], publisher="Yaroslav Mudryi National Law University", seal=False, license="CC BY", apc=True))
+    ]
+
+def test_getEntityById(correct_inputs_and_outputs):
+    result=getEntityById(id)
+    if result == expected:
+        return True
+    else:
+        return False
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def test_getAllJournals():
     pass
