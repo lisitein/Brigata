740ac7561298c158a629e8655114844ea9cc2e41
lisitein <167685239+lisitein@users.noreply.github.com>
2025-12-06 15:31:31 +0100

    Refactor tests and add journal data structure

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ from li import *
 from Yang import *
 from baseHandler import *
 
-
+j=[
+    Journal(title = "Prolíngua", id = "1983-9979", language = "Portuguese", publisher = "Universidade Federal da Paraíba", seal=False, license="CC BY-NC-SA", apc=False),
+    Journal(title = "Проблеми Законності", id = ["2224-9281","2414-990X"] language = ["Ukrainian", "Russian", "English"], publisher = "Yaroslav Mudryi National Law University", seal=False, license="CC BY", apc=True),
+    Journal(title = "Enlightening Tourism: A Pathmaking Journal", id = "2174-548X", language = "English", publisher = "University of Huelva", seal=False, license="CC BY-NC", apc=False),
+    Journal(title = "Scientific Journals of the Maritime University of Szczecin", id = ["1733-8670","2392-0378"], language = "English", publisher = "MUS", seal=False, license="CC BY", apc=True),
+    Journal(title = "Fronteiras: Journal of Social, Technological and Environmental Science", id = "2238-8869", language = "Portuguese", publisher = "Centro Universitário de Anápolis", seal=False, license="CC BY-NC", apc=False),
+    Journal(title = "Semina: Ciências Agrárias", id = ["1676-546X","1679-0359"], language = ["Portuguese", "English"], publisher = "Universidade Estadual de Londrina", seal=False, license="Publisher's own license", apc=True)
+    ]
 
 #TEST 1
 
@@ -46,26 +53,16 @@ def test_getEntityById(correct_inputs_and_outputs):
     for pair in correct_inputs_and_outputs:
         i = i+1
         if getEntityById(pair["input"])==pair["output"]:
-            print(True, "Yoho^^ for getEntityById[i]")
+            print(True, "Yoho^^ for Test1, [i]")
         else:
-            print(False, "Eha... The problem occur in test_getEntityById (TEST1), pair[i]")
+            print(False, "Eha... The problem occurs in test_getEntityById (TEST1), pair[i]")
 
 
 
 #TEST 2
-
-correct_inputs_and_outputs=[
-    Journal(title = "Prolíngua", id = "1983-9979", language = "Portuguese", publisher = "Universidade Federal da Paraíba", seal=False, license="CC BY-NC-SA", apc=False),
-    Journal(title = "Проблеми Законності", id = ["2224-9281","2414-990X"] language = ["Ukrainian", "Russian", "English"], publisher = "Yaroslav Mudryi National Law University", seal=False, license="CC BY", apc=True),
-    Journal(title = "Enlightening Tourism: A Pathmaking Journal", id = "2174-548X", language = "English", publisher = "University of Huelva", seal=False, license="CC BY-NC", apc=False),
-    Journal(title = "Scientific Journals of the Maritime University of Szczecin", id = ["1733-8670","2392-0378"], language = "English", publisher = "MUS", seal=False, license="CC BY", apc=True),
-    Journal(title = "Fronteiras: Journal of Social, Technological and Environmental Science", id = "2238-8869", language = "Portuguese", publisher = "Centro Universitário de Anápolis", seal=False, license="CC BY-NC", apc=False),
-    Journal(title = "Semina: Ciências Agrárias", id = ["1676-546X","1679-0359"], language = ["Portuguese", "English"], publisher = "Universidade Estadual de Londrina", seal=False, license="Publisher's own license", apc=True)
-    ]
-
-def test_getAllJournals(correct_inputs_and_outputs):
+def test_getAllJournals(all_journals):
     if getAllJournals()== correct_inputs_and_outputs:
-        print(True, "Yoho!^^ getAllJournals is True")
+        print(True, "Yoho!^^ getAllJournals(TEST2) is True")
     else:
         print(False, "Eha... The result of getAllJournals (TEST2) is uncorrect.")
 
@@ -109,20 +106,27 @@ correct_inputs_and_outputs=[
 
 
 def test_getJournalsWithTitle(correct_inputs_and_outputs):
+    i = 0
     for pair in correct_inputs_and_outputs:
-    if getJournalsWithTitle(pair["input"])==pair["output"]:
-        print(True, "Yoho^^ getJournalsWithTitle (TEST3) is correct")
-    else:
-        print(False, "Eha... getJournalsWithTitle (TEST3) is uncorrect.")
-
-
-
+        i = i+1
+        if getJournalsWithTitle(pair["input"])==pair["output"]:
+            print(True, "Yoho^^ for TEST3, [i]")
+        else:
+            print(False, "Eha... problem occurs in getJournalsWithTitle (TEST3), pair[i].")
 
 
 #TEST 4
-
+inputs = ["Universi","University","Universidade","MUS"]
+outputs = [[j[0],j[1], j[2],j[4],j[5]],[j[1],j[2]],[j[0],j[5]],[j[3]]]
 def test_getJournalsPublishedBy():
-    pass
+    i = 0
+    for i < 4:
+        if getJournalsPublishedBy(inputs[i]) == outputs[i]:
+            i = i+1
+            print(True, "Yoho^^ for TEST4, [i]") 
+        else:
+            i = i+1
+            print(False, "Eha... problem occurs in getJournalsPublishedBy (TEST4), pair[i].")
 
 
 
@@ -169,6 +173,7 @@ def test_getJournalsInAreasWithLicense():
 def test_getDiamondJournalsInAreasAndCategoriesWithQuartile():
 
     pass
+
 
 
 
