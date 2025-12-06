3ad9b3039383ce8a0d9843e97ad4f0137d856643
danielecamagnadhdk <daniele.camagna@studio.unibo.it>
2025-12-06 12:55:36 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,51 @@
+from laura import *
+from daniele import *
+from li import *
+from Yang import *
+from baseHandler import *
+
+def test_getEntityById(id, expected):
+    result=getEntityById()
+    pass
+
+def test_getAllJournals():
+    pass
+
+def test_getJournalsWithTitle():
+    pass
+
+def test_getJournalsPublishedBy():
+    pass
+
+def test_getJournalsWithLicense():
+    pass
+
+def test_getJournalsWithAPC():
+    pass
+
+def test_getJournalsWithDOAJSeal():
+    pass
+
+def test_getAllCategories():
+    pass
+
+def test_getAllAreas():
+    pass
+
+def test_getCategoriesWithQuartile():
+    pass
+
+def test_getCategoriesAssignedToAreas():
+    pass
+
+def test_getAreasAssignedToCategories():
+    pass
+
+def test_getJournalsInCategoriesWithQuartile():
+    pass
+
+def test_getJournalsInAreasWithLicense():
+    pass
+
+def test_getDiamondJournalsInAreasAndCategoriesWithQuartile():
+    pass
