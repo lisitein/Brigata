716da9fbc269680d0ec282df229d0aad3cb60194
lisitein <167685239+lisitein@users.noreply.github.com>
2026-03-11 16:00:02 +0100

    original li.py

--- li.py
+++ li.py
@@ -1,103 +1,127 @@
-from baseHandler import UploadHandler
+from baseHandler import  UploadHandler 
 from rdflib import Graph, URIRef, Literal, Namespace
 from rdflib.namespace import RDF, XSD
 import pandas as pd
 
+#implements the method of the superclass to handle the specific scenario
+#JournalUploadHandler to handle CSV files in input and to store their data in a graph database
 class JournalUploadHandler(UploadHandler):
     def __init__(self):
         super().__init__()
 
     def pushDataToDb(self, path):
-        base_url = Namespace("https://brigata.github.org/")
-        self.graph = Graph()
-        self.graph.bind("base_url", base_url)
+        base_url = Namespace("https://brigata.github.org/")  
+        self.graph = Graph()  # Create a new RDF graph
+        self.graph.bind("base_url", base_url)  # Bind the base URL to the graph
 
-        journal = pd.read_csv(
-            path,
-            sep=',',
-            encoding='utf-8',
-            keep_default_na=False,
-            names=['title', 'issn', 'eissn', 'languages', 'publisher', 'seal', 'license', 'apc'],
-            header=0,
-            dtype=str
-        )
-
-        id_cols = ['issn', 'eissn']
+        journal = pd.read_csv(path, sep=',', encoding='utf-8',
+                              keep_default_na=False,
+                              names=['title', 'issn', 'eissn', 
+                                    #  'categories', 'areas',
+                                     'languages', 'publisher', 'seal', 'license','apc'],
+                              header=0,
+                              dtype={
+                                  "Journal title":str,
+                                  "Journal ISSN (print version)":str,
+                                  "Journal EISSN (online version)":str,
+                                  "Languages in which the journal accepts manuscripts":str,
+                                  "Publisher":str,
+                                  "DOAJ Seal":bool,
+                                  "Journal license":str,
+                                  "APC":bool
+                                  }) #Read the CSV file into a pandas DataFrame and change the columns' name
+                            
+        # print(journal)                       
+        # subj wil be
+        # id string [1..*]->Journal ISSN (print version),Journal EISSN (online version)
+        # each will be id of the journal(if both ISSN and EISSN), and there will be two subj - one entity
+        # journal_id = {} 
+        id_cols = ['issn','eissn']
         attribute_cols = ['title', 'languages', 'publisher', 'seal', 'license', 'apc']
-
         for idx, row in journal.iterrows():
-            local_id = f"journal-{idx}"
-            subject = URIRef(base_url[local_id])
-            self.graph.add((subject, RDF.type, URIRef(base_url["Journal"])))
-
+            local_id = "journal_" + str(idx)  # internalId??
+            subject = URIRef(base_url[local_id]) # can automatically deal with the URL
+            self.graph.add((subject, RDF.type, URIRef(base_url["Journal"]))) # add type
+        
+        # attributes will be:
+        # Journal title, title: string (1)  -title = URIRef("https://schema.org/name")
+        # Languages in which the journal accepts manuscripts, languages : string [1..*]
+        # Publisher, publisher : string [0.1] 
+        # DOAJ Seal, seal: boolean [1]
+        # Journal licence, licence : string [1]
+        # APC, apc : boolean [1]
             for column in attribute_cols:
-                attribute = (row.get(column) or "")
-                if attribute is None:
-                    attribute = ""
-                attribute = str(attribute).strip()
-                if not attribute:
-                    continue
-
-                predicate = URIRef(base_url[column])
-
-                if column in ['seal', 'apc']:
-                    booleanvalue = attribute.lower() in ['true', 'yes', '1', 'y', 't']
-                    obj = Literal(booleanvalue, datatype=XSD.boolean)
-                    self.graph.add((subject, predicate, obj))
-                elif column == 'languages':
-                    languages = [lang.strip() for lang in attribute.split(',') if lang.strip()]
-                    for language in languages:
-                        obje = Literal(language)
-                        self.graph.add((subject, predicate, obje))
-                else:
-                    objec = Literal(attribute)
-                    self.graph.add((subject, predicate, objec))
-
+                attribute = str(row[column])
+                if attribute:
+                    predicate = URIRef(base_url[column])
+                    if column in ['seal', 'apc']:
+                        booleanvalue = attribute.lower() in ['true','yes']
+                        obj = Literal(booleanvalue, datatype=XSD.boolean)
+                        self.graph.add((subject, predicate, obj ))
+                    elif column == 'languages':
+                        languages = attribute.split(',')
+                        for language in languages:
+                            obje = Literal(language.strip())
+                            self.graph.add((subject, predicate, obje))
+                    else:
+                        objec = Literal(attribute)
+                        self.graph.add((subject, predicate, objec))
+                    
             for column in id_cols:
-                id_value = (row.get(column) or "").strip()
+                id_value = str(row[column])
                 if id_value:
                     predicate = URIRef(base_url["id"])
                     self.graph.add((subject, predicate, Literal(id_value)))
+# do i need to add relations between Journal and Category\ Journal and Area?
+# Jounal - hasCategory -> Category (but Journal has Area?? what is area?)
+        # return self.graph
 
-            if 'categories' in journal.columns:
-                cats = (row.get('categories') or "").strip()
-                if cats:
-                    for cat in [c.strip() for c in cats.split(',') if c.strip()]:
-                        self.graph.add((subject, URIRef(base_url["hasCategory"]), Literal(cat)))
+        # Save the graph to a file or database - I don't know if it is necessary to do it
+        # self.graph.serialize(destination="journal_data.rdf", format="xml")
 
-            if 'areas' in journal.columns:
-                ars = (row.get('areas') or "").strip()
-                if ars:
-                    for ar in [a.strip() for a in ars.split(',') if a.strip()]:
-                        self.graph.add((subject, URIRef(base_url["hasArea"]), Literal(ar)))
-
+        # store and populate a graph database
         from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
         store = SPARQLUpdateStore()
-        endpoint = self.getDbPathOrUrl()
+        endpoint =self.getDbPathOrUrl()
+        # endpoint = "http://127.0.0.1:9999/blazegraph/sparql"  # SPARQL endpoint URL
         if not endpoint:
             print("Error: No database URL set. Call setDbPathOrUrl() first.")
             return False
+        
+        store.open((endpoint, endpoint))  # Open the SPARQL store
 
-        store.open((endpoint, endpoint))
+        # instead of committing one by one (so slow), use SPARQL
+        insert_query = "INSERT DATA {\n" 
+        # Graph <urn:sofia:import:22-02-v2> {\n"
 
-        insert_query = "INSERT DATA {\n"
         for triple in self.graph.triples((None, None, None)):
-            s = triple[0].n3()
-            p = triple[1].n3()
-            o = triple[2].n3()
-            insert_query += f"{s} {p} {o} .\n"
-        insert_query += "}"
+            subject = triple[0]
+            predicate = triple[1] 
+            object_value = triple[2]
+            
 
+            text_value = str(object_value)
+            
+            text_value = text_value.replace('\\', '\\\\')  # \
+            text_value = text_value.replace('"', '\\"')    # ""
+            text_value = text_value.replace('\n', '\\n')   # \n
+            text_value = text_value.replace('\r', '\\r')   # \r
+            
+            line = f"{subject.n3()} {predicate.n3()} {object_value.n3()} .\n"
+            # line = "<" + str(subject) + "> <" + str(predicate) + "> \"" + text_value + "\" .\n"
+           
+
+            insert_query = insert_query + line
+
+        # end the query
+        insert_query = insert_query + "}"
+
+        # store every triples once
         if len(self.graph) > 0:
             store.update(insert_query)
 
+        # Close the store connection
         store.close()
+
         return True
-        
-
-#11111test
-
-# sofia=JournalUploadHandler()
-# sofia.setDbPathOrUrl("http://192.168.1.82:9999/blazegraph/")
-# sofia.pushDataToDb("data/doaj.csv")
