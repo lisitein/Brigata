8e37cf75808e1079eb32d08ba81cdb2c51262c0a
danielecamagnadhdk <daniele.camagna@studio.unibo.it>
2026-03-16 18:20:32 +0100

    changes

--- t.py
+++ t.py
@@ -1,5 +1,5 @@
 from impl import *
 sophie=JournalUploadHandler()
-sophie.setDbPathOrUrl('http://192.168.1.115:9999/blazegraph/')
+sophie.setDbPathOrUrl('http://127.0.0.1:9999/blazegraph/sparql')
 sophie.pushDataToDb('data/doaj.csv')
 print('Done')
