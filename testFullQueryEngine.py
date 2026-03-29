385136aa2d1cfa590d269de2fb0dac0c15de51d4
danielecamagnadhdk <daniele.camagna@studio.unibo.it>
2026-03-29 20:45:40 +0200

    upd

--- testFullQueryEngine.py
+++ testFullQueryEngine.py
@@ -63,7 +63,7 @@ rel_path = "data/relational_database.db"
 cat = CategoryUploadHandler()
 cat.setDbPathOrUrl(rel_path)
 
-grp_endpoint = "http://172.20.10.2:9999/blazegraph/sparql"
+grp_endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
 jou = JournalUploadHandler()
 jou.setDbPathOrUrl(grp_endpoint)
 
--- testQueryEngine.py
+++ testQueryEngine.py
@@ -57,7 +57,7 @@ rel_path = "data/relational_database.db"
 cat = CategoryUploadHandler()
 cat.setDbPathOrUrl(rel_path)
 
-grp_endpoint = "http://10.42.131.65:9999/blazegraph/sparql"
+grp_endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
 jou = JournalUploadHandler()
 jou.setDbPathOrUrl(grp_endpoint)
 
